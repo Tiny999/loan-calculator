51af5a2643c1d2b8108b4fc2e9d23ae68968874a
Abbas Salami <olawalesalami3@gmail.com>
2021-01-28 15:19:18 +0100

    finish

--- app.js
+++ app.js
@@ -1,7 +1,62 @@
+const num2text = {
+    ones: ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen'],
+    tens: ['', '', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety'],
+    sep: ['', ' Thousand, ', ' Million, ', ' Billion, ', ' Trillion, ', ' Quadrillion, ', ' Quintillion, ', ' Sextillion, ']
+  };
+  const convert = function(val) {
+    if (val.length === 0) {
+      return '';
+    }
+  
+    val = val.replace(/,/g, '');
+    if (isNaN(val)) {
+      return 'Invalid input.';
+    }
+  
+  
+    let [val1, val2] = val.split(".")
+    let str2 = "";
+    if (val2 != null && val2 != '') {
+      //convert the decimals here
+      var digits = (val2+"0").slice(0,2).split("");
+      str2 = num2text.tens[+digits[0]] + " " + num2text.ones[+digits[1]]
+    }
+    let arr = [];
+    while (val1) {
+      arr.push(val1 % 1000);
+      val1 = parseInt(val1 / 1000, 10);
+    }
+    let i = 0;
+    let str = "";
+    while (arr.length) {
+      str = (function(a) {
+        var x = Math.floor(a / 100),
+          y = Math.floor(a / 10) % 10,
+          z = a % 10;
+  
+        return (x > 0 ? num2text.ones[x] + ' Hundred and ' : '') +
+          (y >= 2 ? num2text.tens[y] + ' ' + num2text.ones[z] : num2text.ones[10 * y + z]);
+      })(arr.shift()) + num2text.sep[i++] + str;
+    }
+  
+    return str +
+      ' Naira' +
+      (str2 ? ', ' + str2 + ' Kobo' : '') +
+      ' Only';
+  };
+  
+
+
 Date.prototype.addDays = function(days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
+}
+
+Date.prototype.formatMMDDYYYY = function(){
+    return  this.getDate() +
+    "/" + (this.getMonth() + 1) + 
+    "/" +  this.getFullYear();
 }
 
 document.querySelector('#calc-form').addEventListener('submit', function(e){
@@ -11,6 +66,9 @@ document.querySelector('#calc-form').addEventListener('submit', function(e){
     const loanAmount = parseFloat(document.querySelector('#amount').value);
     const interest = parseFloat(document.querySelector('#interest').value);
     const totalAmount = loanAmount + interest;
+
+    const amountInWords = convert(`${totalAmount}`);
+
     const dateDisburse = new Date(document.querySelector('#dateDisburse').value);
     const periodRepay = document.querySelector('#periodRepay').value;
 
@@ -46,7 +104,7 @@ document.querySelector('#calc-form').addEventListener('submit', function(e){
             </tr>
             <tr>
                 <th colspan="2">AMOUNT (In words):</th>
-                <th colspan='4'>One Million, Three Hundred And Seventeen Thousand, Nine Hundred & Fourty One Naira, Twenty One Kobo</th>
+                <th colspan='4'>${amountInWords}</th>
             </tr>
             <tr>
                 <th colspan="2">Date of Disbursment</th>
@@ -83,19 +141,23 @@ document.querySelector('#calc-form').addEventListener('submit', function(e){
         </tfoot>
     `;
 
-    // let currentDate = new Date(document.querySelector('#dateDisburse').value);
+
+
+    let currentDate = dateDisburse.addDays(7);
 
     for( let i = 1; i <= parseInt(periodRepay); i++ ){
         const amount = loanAmount / parseInt(periodRepay);
         const int = interest / parseInt(periodRepay);
         const total = int + amount;
+
+        const date = currentDate.formatMMDDYYYY()
         
 
         const tr = document.createElement('tr');
 
         tr.innerHTML = `
             <th scope="row">${i}</th>
-            <td>29/06/20</td>
+            <td>${date}</td>
             <td>${amount.toLocaleString(undefined, {'minimumFractionDigits':2, 'maximumFractionDigits':2})}</td>
             <td>${int.toLocaleString(undefined, {'minimumFractionDigits':2, 'maximumFractionDigits':2})}</td>
             <td>${total.toLocaleString(undefined, {'minimumFractionDigits':2, 'maximumFractionDigits':2})}</td>
@@ -103,6 +165,8 @@ document.querySelector('#calc-form').addEventListener('submit', function(e){
         `;
 
         document.querySelector('#tab table tbody').appendChild(tr);
+
+        currentDate = currentDate.addDays(7);
 
     } 
 
